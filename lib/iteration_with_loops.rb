542393cbbb16ad95d89674d11faa35d92ab94f13
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-13 23:54:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,10 +3,10 @@ def find_even_values(src)
   #
   # Output all even values in each nested array
   row_index = 0
-  while row_index < spice_rack.count do
+  while row_index < src.count do
     element_index = 0
-    while element_index < spice_rack[row_index].count do
-      p spice_rack[row_index][element_index]
+    while element_index < src[row_index].count do
+      p src[row_index][element_index]
       element_index += 1
     end
     row_index += 1
